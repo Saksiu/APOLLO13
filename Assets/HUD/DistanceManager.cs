a657140e6bfef35f37683f705472e8abca48bb52
“Yaro” <s25509@pjwstk.edu.pl>
2024-11-30 22:30:23 +0100

    HUD working + eagles prefabs

--- Assets/HUD/DistanceManager.cs
+++ Assets/HUD/DistanceManager.cs
@@ -3,26 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class DistanceManager : Singleton<DistanceManager>
 {
-    //[ReadOnlyField] (??)
-    [SerializeField] private int _distanceTraveled;
     private const int MoonTotalDistance = 384400000;
+    [SerializeField] private int _distanceTraveled;
+    [SerializeField] private TextMeshProUGUI _meterDistance;
 
     public string GetDistanceFormatted()=>FormatToDistance(_distanceTraveled);
 
     public string FormatToDistance(int totalDistance)
     {
-        if (gameObject.transform.parent.gameObject.name == "DistanceToMoon")
-            totalDistance = MoonTotalDistance - totalDistance;
+        int distanceLeft = MoonTotalDistance - totalDistance;
 
-        int meters = Mathf.FloorToInt(totalDistance / 1000F);
-        int kilometers = Mathf.FloorToInt(totalDistance - meters * 1000);
-        return string.Format("{0:0000}:{1:000}", kilometers, meters); //TODO: verify format
+        int meters = Mathf.FloorToInt(distanceLeft / 1000F);
+        int kilometers = Mathf.FloorToInt(distanceLeft - meters * 1000);
+        return string.Format("Distance to moon: /n {0:000000}km;{1:000}m", kilometers, meters); //TODO: verify format
     }
     
-    [SerializeField] private TextMeshProUGUI _timerText;
     protected override void Awake()
     {
         base.Awake();
