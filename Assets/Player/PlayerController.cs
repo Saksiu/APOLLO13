971c4b397505a3edd5c83ec8a6c6b982326410dc
unknown <nowywy@gmail.com>
2024-11-30 17:58:07 +0100

    fuel component implementation

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,4 +1,5 @@
 
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Vector2 = UnityEngine.Vector2;
@@ -9,13 +10,17 @@ public class PlayerController : Singleton<PlayerController>, PlayerInputActions.
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private bool canMoveVertical = false;
 
+
     [SerializeField] private Collider playerMoveBounds;
+
+    [NonSerialized]public PlayerFuelComponent FuelComponent;
     private Rigidbody rb;
     
     
     // Start is called before the first frame update
     void Start()
     {
+        FuelComponent = GetComponent<PlayerFuelComponent>();
         rb = GetComponent<Rigidbody>();
         PlayerInputManager.PlayerInput.Player.SetCallbacks(this);
         PlayerInputManager.PlayerInput.Player.Enable();
@@ -24,9 +29,16 @@ public class PlayerController : Singleton<PlayerController>, PlayerInputActions.
     // Update is called once per frame
     void FixedUpdate()
     {
+        if(!FuelComponent.HasFuel)
+            return;
+        
         Vector2 moveInput = PlayerInputManager.PlayerInput.Player.Move.ReadValue<Vector2>();
         Vector3 finalMoveVector= new Vector3(moveInput.x, 0, moveInput.y);
         finalMoveVector *= moveSpeed;
+        
+        if (!canMoveVertical)
+            finalMoveVector.z=0;
+        
         if (!playerMoveBounds.bounds.Contains(rb.position + finalMoveVector))
         {
             rb.velocity=(Vector3.zero);
--- Assets/Player/PlayerFuelComponent.cs
+++ Assets/Player/PlayerFuelComponent.cs
@@ -1,18 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerFuelComponent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Tooltip("Per second")]
+    [SerializeField] private float fuelConsumptionRate = 5.0f;
+    
+    [SerializeField] private float initialFuel = 100.0f;
+    public float CurrentFuel = 100.0f;
+    
+    public bool HasFuel => CurrentFuel > 0.0f;
+    
+    private void Start()
     {
-        
+        CurrentFuel = initialFuel;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if(!HasFuel) return;
+        CurrentFuel -= (fuelConsumptionRate * Time.fixedDeltaTime);
+        if (CurrentFuel < 0.0f)
+        {
+            OnNoFuelLeft();
+        }
+    }
+    
+    private void OnNoFuelLeft()
+    {
+        Debug.Log("No fuel left");
     }
 }
